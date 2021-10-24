204bf566cc8ddbeaf501485904d57f7df14889e6
Mildivar <pac-man@yandex.ru>
2021-10-24 23:22:06 +1000

    feature/lesson3_1 created

--- src/main/kotlin/VideoAttachment.kt
+++ src/main/kotlin/VideoAttachment.kt
@@ -1,7 +1,5 @@
 //class VideoAttachment: Attachments<Video> ()
 
 data class VideoAttachment(var video: Video) : Attachments("video") {
-//    fun addNNN() {
-//        video = listOf(Video())
-//    }
+
 }
